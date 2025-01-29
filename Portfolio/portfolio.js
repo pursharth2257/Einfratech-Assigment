da2378ad228880600084431145531d47a757a1c1
Pursharth Chaudhary <pursharth.chaudhary2257@gmail.com>
2025-01-30 01:07:19 +0530

    Second Project

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,12 @@
+
+const darkModeToggle = document.getElementById("darkModeToggle");
+darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    darkModeToggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
+
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Message sent successfully!");
+});
